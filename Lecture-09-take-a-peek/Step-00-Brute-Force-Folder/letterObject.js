51ce5a29d62b09911e6e2807a158f73eb6ef827d
NeuTrix <neutrix@brandgeneering.com>
2017-04-04 17:15:11 -0700

    Restructure raw code file for bugs

---
+++ Lecture-09-take-a-peek/Step-00-Brute-Force-Folder/letterObject.js
@@ -0,0 +1,39 @@
+// make a letter object constructor
+
+var Letter = function(name) {
+	this.name = name;
+	this.status = "unchosen";
+	this.color = "orange";
+	this.background = "white"
+}
+
+// make a KeyButton conversion function
+var keyButton = function(letter) {
+	var newName = letter.name
+	var button = function letter["newName"]:letter};
+	return button;
+}
+
+var keyboard = []
+
+var keyButton = function(name,letter) {
+	// var newName = letter.name
+
+	var button = {name:letter};
+	keyboard.push(button);
+}
+
+// make a keyboard object constructor of letters
+var Keyboard = function(letter){
+	// create a function to make letter objects for the hash
+
+
+	this.letters = [];
+
+}
+
+// make prototype methods ?
+
+// create DOM obejct variables to connect and disply them
+
+// create fns to manipulate the objects
